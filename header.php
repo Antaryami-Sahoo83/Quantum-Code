8bd2c0d57e2d8f3de122c3f2e8b9fc6effa92622
SubhranShu2332 <124662904+SubhranShu2332@users.noreply.github.com>
2023-12-20 16:29:52 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <link rel="icon" href="../../resources/logo.png" type="image/x-icon" />
+  <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+
+  <!-- my Js -->
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+<body>
+<div class="tle fixed-top">
+    <center class="d-block d-sm-none mt-1 ">
+      <h1>Quantum Code</h1>
+    </center>
+    <nav class="navbar bg-body-white navbar-expand-lg">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Quantum Code</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <a class="navbar-brand d-none d-lg-block" href="#">Quantum Code</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <form class="d-flex d-block d-sm-none" role="search">
+                <input class="form-control me-2 " type="search" placeholder="  🔍  Search For The Courses"
+                  aria-label="Search">
+              </form>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./career.html">Courses</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Careers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Placements</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+              </li>
+              <form class="d-flex ms-5 mt-1 d-none d-lg-block" role="search">
+                <input class="form-control me-2 " type="search" placeholder="  🔍       Search For The Courses..."
+                  aria-label="Search">
+              </form>
+            </ul>
+            <button class="btn btn-outline-dark ms-3 rounded-1" type="submit">Log in</button>
+            <button class="btn btn-dark ms-3 rounded-1" type="submit">Sign up</button>
+          </div>
+        </div>
+    </nav>
+  </div>
+</body>
+</html>
